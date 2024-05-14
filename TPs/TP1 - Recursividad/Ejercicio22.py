467f6e583a67dc669b75b66d7db9cefc76aef2ac
= <=>
2024-05-14 17:54:04 -0300

    TP 1

---
+++ TPs/TP1 - Recursividad/Ejercicio22.py
@@ -0,0 +1,23 @@
+#El problema de la mochila Jedi. Suponga que un Jedi (Luke Skywalker, Obi-Wan Kenobi, Rey u otro, el que más le guste) 
+#está atrapado, pero muy cerca está su mochila que contiene muchos objetos.
+
+#Implementar una función recursiva llamada “usar la fuerza” que le permita al Jedi “con ayuda de la fuerza” realizar las siguientes actividades:
+
+#a. sacar los objetos de la mochila de a uno a la vez hasta encontrar un sable de luz o que no queden más objetos en la mochila;
+#b. determinar si la mochila contiene un sable de luz y cuantos objetos fueron necesarios sacar para encontrarlo;
+#c. Utilizar un vector para representar la mochila.
+
+mochila = ['pistola', 'botella', 'vendas','sable de luz']
+
+def usar_la_fuerza(mochila, index=0, contador=0):
+    if index >= len(mochila):
+        return ('No se encontró el sable de luz')
+    elif mochila[index] == "sable de luz":
+        return ('Se encontró el sable de luz', contador+1)
+    else:
+        return usar_la_fuerza(mochila, index+1, contador+1)
+
+print(usar_la_fuerza(mochila))
+
+
+        
